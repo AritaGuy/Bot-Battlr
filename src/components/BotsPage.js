9449875f44d29f631cf2608020b3186aeaae826c
Sam Arita <arita.sam@student.moringaschool.com>
2022-10-03 11:38:37 +0300

    Passed prop

--- src/components/BotsPage.js
+++ src/components/BotsPage.js
@@ -1,14 +1,21 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import YourBotArmy from "./YourBotArmy";
 import BotCollection from "./BotCollection";
 
 function BotsPage() {
   //start here with your code for step one
+  const[bots, setBots]=useState([])
+  useEffect(()=>{
+    fetch('http://localhost:8002/bots')
+    .then((r)=>r.json())
+    .then((bot)=>setBots(bot))
+  }, [])
+  
 
   return (
     <div>
       <YourBotArmy />
-      <BotCollection />
+      <BotCollection collection={bots}/>
     </div>
   )
 }
